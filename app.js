a13ec6cc0e6e0ef955daa5dfcf599abd15d5fce1
Marius <marius@example.com>
2022-02-09 17:05:22 +0200

    init fe

---
+++ app.js
@@ -0,0 +1,19 @@
+let townsArr = ['London', 'Kaunas', 'Siauliai'];
+
+const buttonsArr = document.querySelectorAll('.controll button');
+
+buttonsArr.forEach((el) =>
+  el.addEventListener('click', (e) => {
+    const clickedTownName = e.target.textContent;
+    console.log(clickedTownName);
+
+    if (townsArr.includes(clickedTownName)) {
+      e.target.classList.remove('active');
+      townsArr = townsArr.filter((t) => t !== clickedTownName);
+    } else {
+      e.target.classList.add('active');
+      townsArr.push(clickedTownName);
+    }
+    console.log('townsArr ===', townsArr);
+  })
+);
